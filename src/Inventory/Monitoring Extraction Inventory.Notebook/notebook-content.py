a588539cc435af9869df9fa5add1955b012eb98a
Edgar Cotte <edgar.cotte@microsoft.com>
2025-07-18 22:53:01 +0200

    Update on Notebooks

--- src/Inventory/Monitoring Extraction Inventory.Notebook/notebook-content.py
+++ src/Inventory/Monitoring Extraction Inventory.Notebook/notebook-content.py
@@ -33,14 +33,14 @@
 
 # CELL ********************
 
-cluster_ingest = "https://ingest-trd-uu4zdj3cr1r15y4vxc.z2.kusto.fabric.microsoft.com"
-cluster_query = "https://trd-uu4zdj3cr1r15y4vxc.z2.kusto.fabric.microsoft.com"
-database_name = 'Platform Inventory'
+cluster_ingest = ""
+cluster_query = ""
+database_name = ''
 
-key_vault_uri = f"https://mrtacatkeyvault.vault.azure.net/"
-key_vault_tenant_id = f"tenant-id"
-key_vault_client_id = f"fabric-admin-api-sp-id"
-key_vault_client_secret = f"fabric-admin-api-sp-secret"
+key_vault_uri = f""
+key_vault_tenant_id = f""
+key_vault_client_id = f""
+key_vault_client_secret = f""
 
 # METADATA ********************
 
